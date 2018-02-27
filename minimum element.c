6157df5240f531bb19daa19e47bbb4762ddd6915
smartkarthik <35652317+smartkarthik@users.noreply.github.com>
2018-02-27 16:15:05 +0530

    Create minimum element.c

---
+++ minimum element.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int a[50],b,c,i;
+    printf("\nenter the N value");
+    scanf("%d",&b);
+    printf("\nenter the arry value");
+    for(i=0;i<b;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+        c=a[0];
+       for(i=0;i<b;i++) 
+       {
+    if(a[i]<c)
+    {
+        c=a[i];
+    }
+    printf("The minimum  element is %d",c);
+
+}
+}
